0a5ba40ea8243ba1a61c986cbafea8b95cecc097
gentleSlop3s <angelocyril65@gmail.com>
2026-02-26 14:40:19 +0300

    Initial commit

--- SokoApp/urls.py
+++ SokoApp/urls.py
@@ -7,12 +7,14 @@ from SokoApp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('home/', views.home),
+    path('home/', views.home, name='home'),
 
-    path('contact/', views.contact),
+    path('contact/', views.contact, name='contact'),
 
-    path('about/', views.about),
+    path('about/', views.about, name='about'),
 
-    path('services/', views.services)
+    path('services/', views.services, name='services')
+
+    
 
 ]
